82d77404cf8d1530696441d9e48e5f8accd688c0
morot <morot3060@gmail.com>
2016-11-29 16:28:50 +0900

    main

--- Sender/MainTest.py
+++ Sender/MainTest.py
@@ -1,10 +1,16 @@
 from Sender import Main
+import sys
 
-main= Main.Main()
+main = Main.Main()
 
-##set verifying key, json data, template parameter
-##edit the parameters (smtp login)
-main.setSMTP(yourID,youPW)
-##edit the parameters (verifying key, json data, html template) - string
-main.setInfor(yourVarifyingKey,'{"sender":yourEMAIL, "receiver":othersEMAIL, "subject":yourSUBJECT}',HTMLtemplate)
+# set verifying key, json data, template parameter
+# edit the parameters (smtp login)
+
+main.setSMTP(sys.argv[1],sys.argv[2])
+
+# edit the parameters (verifying key, json data, html template) - string
+
+# main.setInfor(yourVarifingKey,'{"sender":yourEMAIL, "receiver":othersEMAIL, "subject":SUBJECT}',HTMLparams)
+main.setInfor(sys.argv[3],sys.argv[4],sys.argv[5])
+
 main.run()
